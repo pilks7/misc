e909cf246170e22b98e6d80de78d4bff486dbe9b
pilks7 <7pilks7@gmail.com>
2024-01-18 11:43:37 +0000

    Update checksum.sh

--- checksum.sh
+++ checksum.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-VAR1="add6cb45aed969a49b21322fbd2e34c896f2a618d2a9eb8c865a05602365ef6c"
-VAR2="add6cb45aed969a49b21322fbd2e34c896f2a618d2a9eb8c865a05602365ef6c"
 
-if [ "$VAR1" = "$VAR2" ]; then
+if [ "$1" = "$2" ]; then
     echo "Strings are equal."
 else
     echo "Strings are not equal."
